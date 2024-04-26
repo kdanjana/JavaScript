bddaf905b0c4f14622e9ff75d613686748d5e8df
anjana <kad.an.devi@gmail.com>
2024-04-26 15:10:07 +0530

    basics

---
+++ basics/index.js
@@ -0,0 +1,29 @@
+console.log("from js file");
+
+// varibale declaration
+let b;
+//assignment
+b = 10;
+//variable declaration and assignment
+let a = 10;
+console.log(a);
+//variable re-assignment
+a = 20;
+console.log(a);
+a = "abc";
+console.log(a);
+//backticks
+let name = "dog";
+console.log('My name is ${name}');  // output: My name is ${name}
+console.log(`My name is ${name}`);  // output: My name is dog
+console.log(`1+2 is ${1+2}`);      // 1+2 is 3
+
+//undefined
+let age;
+console.log(age);   // output - undefined
+
+//null
+let count = null;
+console.log(count);   // output -- null
+
+
