ebd6a8eb1859047eeb805a47cfbc3d87b6aa215b
anjana <kad.an.devi@gmail.com>
2024-04-28 18:22:41 +0530

    calculator - Js, HTML, CSS

---
+++ calculator/app.js
@@ -0,0 +1,19 @@
+let defaultResult = 0;
+let currentResult = defaultResult;
+
+//function to add numbers
+function addNumbers(num1, num2){
+    return num1 + num2;
+}
+
+//function to subtract numbers
+function subtractNumbers(num1, num2){
+    return num1 - num2;
+}
+
+//function to multiply numbers
+function multiplyNumbers(num1, num2){
+    return num1 * num2;
+}
+
+currentResult = addNumbers(1,2);
